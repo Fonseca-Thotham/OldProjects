09ca37852c3aca471c7aaa3fb38691dc62a02130
Fonseca <matheusfonsecade@hotmail.com>
2022-06-27 16:58:05 -0300

    Requisito 8 Concluído

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -3,17 +3,39 @@ import PropTypes from 'prop-types';
 import Header from './Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from './MusicCard';
+import Loading from './Loading';
+import { addSong, removeSong } from '../services/favoriteSongsAPI';
 
 class Album extends React.Component {
   state = {
     albumSongs: [],
     artist: '',
     album: '',
+    loading: false,
+    favMusic: [],
   }
 
   componentDidMount() {
     this.getSongs();
   }
+
+  addFavorite = async (event, songID) => {
+    const { albumSongs, favMusic } = this.state;
+    this.setState({ loading: true });
+    const favorite = albumSongs.find((song) => (
+      song.trackId === songID
+    ));
+    if (favMusic.includes(favorite)) {
+      const newFavMusic = favMusic.filter((song) => song.trackId !== songID);
+      this.setState({ favMusic: newFavMusic });
+      await removeSong(favorite);
+    }
+    if (!favMusic.includes(favorite)) {
+      favMusic.push(favorite);
+      await addSong(favorite);
+    }
+    this.setState({ loading: false });
+  };
 
   getSongs = async () => {
     const { match: { params: { id } } } = this.props;
@@ -24,27 +46,32 @@ class Album extends React.Component {
   }
 
   render() {
-    const { albumSongs, artist, album } = this.state;
+    const { albumSongs, artist, album, loading } = this.state;
+    const contentScreen = (
+      <section>
+        <div>
+          <h3 data-testid="artist-name">{ artist }</h3>
+          <h1 data-testid="album-name">{ album }</h1>
+        </div>
+        {albumSongs
+          .filter((song) => (
+            song.trackId
+          ))
+          .map((song) => (
+            <MusicCard
+              key={ song.trackId }
+              trackName={ song.trackName }
+              previewUrl={ song.previewUrl }
+              trackId={ song.trackId }
+              loadScreen={ (event) => this.addFavorite(event, song.trackId) }
+            />
+          ))}
+      </section>
+    );
     return (
       <div data-testid="page-album">
         <Header />
-        <section>
-          <div>
-            <h3 data-testid="artist-name">{ artist }</h3>
-            <h1 data-testid="album-name">{ album }</h1>
-          </div>
-          {albumSongs
-            .filter((song) => (
-              song.trackId
-            ))
-            .map((song) => (
-              <MusicCard
-                key={ song.trackId }
-                nomeMusica={ song.trackName }
-                songUrl={ song.previewUrl }
-              />
-            ))}
-        </section>
+        { loading ? <Loading /> : contentScreen}
       </div>
     );
   }
--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -2,25 +2,54 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class MusicCard extends React.Component {
+  state = {
+    isChecked: false,
+  }
+
+  componentDidMount() {
+    const { trackId } = this.props;
+    this.isItChecked(trackId);
+  }
+
+  isItChecked = (id) => {
+    const favSongsList = JSON.parse(localStorage.getItem('favorite_songs'));
+    if (favSongsList.length > 0) {
+      const isIt = favSongsList.some((song) => song.trackId === id);
+      this.setState({ isChecked: isIt });
+    }
+  }
+
   render() {
-    const { nomeMusica, songUrl } = this.props;
+    const { trackName, previewUrl, trackId, loadScreen } = this.props;
+    const { isChecked } = this.state;
     return (
       <section>
-        <p>{ nomeMusica }</p>
-        <audio data-testid="audio-component" src={ songUrl } controls>
+        <p>{ trackName }</p>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
           <track kind="captions" />
           O seu navegador não suporta o elemento
           <code>audio</code>
           .
         </audio>
+        <label htmlFor={ `checkbox-music-${trackId}` }>
+          Favorita
+          <input
+            type="checkbox"
+            checked={ isChecked }
+            data-testid={ `checkbox-music-${trackId}` }
+            onChange={ loadScreen }
+          />
+        </label>
       </section>
     );
   }
 }
 
 MusicCard.propTypes = {
-  nomeMusica: PropTypes.string.isRequired,
-  songUrl: PropTypes.string.isRequired,
+  trackName: PropTypes.string.isRequired,
+  previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.number.isRequired,
+  loadScreen: PropTypes.func.isRequired,
 };
 
 export default MusicCard;
